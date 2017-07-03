a36d0161ead7617f9a26a765af206e38dd7e418d
Kyle Swanson <swansonk.14@gmail.com>
2017-07-03 19:07:01 +0000

    Improve results printing script

--- max_result.py
+++ max_result.py
@@ -1,7 +1,6 @@
-import sys
-import numpy as np
 import process_results
 import argparse
+from pprint import pprint
 
 def main(filename, min_epoch, max_epoch, max_group, max_measure, reverse):
     results = process_results.get_results(filename)
@@ -18,23 +17,19 @@ def main(filename, min_epoch, max_epoch, max_group, max_measure, reverse):
     
     max_min = 'Minimum' if reverse else 'Maximum'
 
-    print
-    print '{} {} {} of {:.3f} on epoch {:d} in epoch range {:d} to {:d}'.format(max_min, max_group, max_measure, max_results[max_group][max_measure], max_results['epoch'], min_epoch, max_epoch)
-    print  '{} {} {} of {:.3f} on epoch {:d} in epoch range {:d} to {:d}'.format(max_min, 'test', max_measure, max_results['test'][max_measure], max_results['epoch'], min_epoch, max_epoch)
-    print
-
-    for group in ['train', 'dev', 'test', 'grid']:
-        try:
-            #print '{} accuracy {:.3f}%'.format(group, 100*max_results[group]['accuracy'])
-            print '{} auc {}'.format(group, max_results[group]['auc'])
-            print '{} f1 {}'.format(group, max_results[group]['f1'])  
-            print '{} precision {}'.format(group, max_results[group]['precision'])  
-            print '{} recall {}'.format(group, max_results[group]['recall'])  
-            print '{} confusion matrix'.format(group)
-            print np.array(max_results[group]['confusion_matrix'])
-            print
-        except Exception as e:
-            pass
+    print('')
+    print('{} {} {} of {:.3f} on epoch {:d} in epoch range {:d} to {:d}'.format(max_min, max_group, max_measure, max_results[max_group][max_measure], max_results['epoch'], min_epoch, max_epoch))
+    print('')
+    print('epoch {}'.format(max_results['epoch']))
+    print('')
+    print('train results')
+    pprint(max_results['train'])
+    print('')
+    print('dev results')
+    pprint(max_results['dev'])
+    print('')
+    print('test results')
+    pprint(max_results['test'])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -43,9 +38,7 @@ if __name__ == '__main__':
     parser.add_argument('--max_epoch', help='epoch to end search; default is last epoch', type=int)
     parser.add_argument('--group', help='train, dev, test, or grid; default is test')
     parser.add_argument('--measure', help='measure to find maximum of; default is f1')
-    parser.add_argument('--reverse', help='True to compute min instead of max', type=str, default='False')
+    parser.add_argument('--reverse', action='store_true', default=False, help='True to compute min instead of max')
     args = parser.parse_args()
 
-    reverse = args.reverse == 'True'
-
-    main(args.filename, args.min_epoch, args.max_epoch, args.group, args.measure, reverse)
+    main(args.filename, args.min_epoch, args.max_epoch, args.group, args.measure, args.reverse)
